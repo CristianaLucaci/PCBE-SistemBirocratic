016686048db7b6d9b865c946bc7266c372c4911b
Ciuban Casian <ciubancasian@yahoo.com>
2021-11-05 17:50:27 +0200

    First implementation for Ghiseu

--- src/main/java/model/Ghiseu.java
+++ src/main/java/model/Ghiseu.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Ghiseu {
+	
+	private Birou birou; // biroul din care face parte ghiseul
+	private int nr; // numarul de identificare al ghiseului, nu pot exista doua ghisee cu acelasi nr, apartinand aceluiasi birou 
+	
+	public Ghiseu(Birou birou, int nr) {
+		this.birou = birou;
+		this.nr = nr;
+	}
+	
+	public int getNr() {
+		return nr;
+	}
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+	
+	public Birou getBirou() {
+		return birou;
+	}
+	public void setBirou(Birou birou) {
+		this.birou = birou;
+	}
+	
 
 }

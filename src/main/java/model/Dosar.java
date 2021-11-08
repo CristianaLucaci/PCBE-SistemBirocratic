fc15d303ba63bb98658f1fe2dba63914512a55be
doritmtm <dorian_tm@yahoo.com>
2021-11-08 14:57:44 +0200

    Initial implementation of Dosar

--- src/main/java/model/Dosar.java
+++ src/main/java/model/Dosar.java
@@ -1,4 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dosar {
+    private int nrDocumente;
+    private List<Document> documente;
+
+    public Dosar() {
+        nrDocumente=0;
+        documente=new ArrayList<>();
+    }
+
+    public Dosar(int nrDocumente, List<Document> documente) {
+        this.nrDocumente = nrDocumente;
+        this.documente = documente;
+    }
+
+    public void addDocument(Document d)
+    {
+        nrDocumente++;
+        documente.add(d);
+    }
+
+    public int getNrDocumente() {
+        return nrDocumente;
+    }
+
+    public void setNrDocumente(int nrDocumente) {
+        this.nrDocumente = nrDocumente;
+    }
+
+    public List<Document> getDocumente() {
+        return documente;
+    }
+
+    public void setDocumente(List<Document> documente) {
+        this.documente = documente;
+    }
 }
